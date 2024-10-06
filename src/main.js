f9903a46b840de1a6da1680268da72baaaa8ede3
huangzhongyuan <huangzhongyuan@shouqianba.com>
2024-10-06 22:51:41 +0800

    fix: 部署

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 import './assets/main.css'
+import ErrorStackParser from 'error-stack-parser'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { findCodeBySourceMap } from './utils'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.config.errorHandler = (err,vm) => {
+    const errorStack = ErrorStackParser.parse(err)
+    findCodeBySourceMap(errorStack[0])
+}
 
 app.mount('#app')
---
+++ src/utils/index.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import sourceMap from 'source-map-js'
+
+const getSourceMap = async (url: string) => {
+    const res = await axios.get(url)
+    return res
+}
+const findCodeBySourceMap = async (stackFrame: any) => {
+    const map = await getSourceMap(stackFrame.filename + '.map')
+    const fileContent = map.data
+    // 解析map文件
+    const consumer = await new sourceMap.SourceMapConsumer(fileContent)
+    // 通过报错位置查找对应源文件的名称和行数
+    const originalPosition = consumer.originalPositionFor({
+        line: stackFrame.lineNumber,
+        column: stackFrame.columnNumber
+    })
+    const code = consumer.sourceContentFor(originalPosition.source)
+    console.log(code, '还原之后的代码')
+}
+
+export {
+    findCodeBySourceMap
+}
