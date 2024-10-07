38cefba5a40d16d92a36bd846ac3eccef4982141
huangzhongyuan <huangzhongyuan@shouqianba.com>
2024-10-07 14:44:30 +0800

    fix: 部署

---
+++ src/views/PreView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="pre-code">
+    <div class="error-detail">
+      <pre class="error-code" v-html=""></pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PreView',
+  props: {
+      origin: Object
+  },
+  methods: {
+        preLine() {
+           const line =  this.origin.line
+           const originCodeLine = this.origin.source.split('\n')
+           const len = originCodeLine.length - 1
+           const start = line - 3 >=0 ? line - 3 : 0
+           const end = start + 5 >=len ? len : start + 5
+           const newLines = []
+           for(let i= start; i<=end; i++) {
+                const content = i +1 + '.   ' + this.encodeHTML(originCodeLine[i])
+                newLines.push(`
+                  <div class="code-line ${i+1 == line ? 'heightlight' : ''}">${content}</div>
+                `)
+           }
+           rerurn newLines.join('')
+        },
+        encodeHTML(str) {
+            if(!str || str.length === 0) return ''
+            return str.replace(/&/g, '&#38;')
+                     .replace(/</g, '&lt;').
+                     replace(/>/g, '&gt;').
+                     replace(/'/g, '&#039;')
+        }
+  }
+}
+</script>
+
+<style scoped>
+.error-code {
+    padding: 10px;
+    overflow: hidden;
+    font-family: consolas, monospace;
+    word-wrap: normal;
+}
+.code-line {
+    padding: 4px;
+}
+.heightlight {
+    color: #fff;
+    background: #f12926;
+}
+</style>
