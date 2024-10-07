89d8a923a280604fa99d9e68925b4286c0f5c5c3
hzy618861 <hzy618861@users.noreply.github.com>
2024-10-07 07:42:07 +0000

    deploy: 31a83941ebde03658af929cfc150519740f81814

---
+++ assets/eventStore-Yf49HsBm.js
@@ -0,0 +1,2 @@
+import{g as t}from"./index-eUL_P5z5.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvents(){return this.events}}});export{n as u};
+//# sourceMappingURL=eventStore-Yf49HsBm.js.map
